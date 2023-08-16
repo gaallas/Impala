d2f9626b9030d9945f67f4f404c882c916d61299
Joe McDonnell <joemcdonnell@cloudera.com>
2023-08-15 21:48:13 -0700

    googletest 1.12.1
    Change-Id: Iec5e369314a4a38270ca5d254c597c7d7e06cb58

--- be/src/exprs/expr-test.cc
+++ be/src/exprs/expr-test.cc
@@ -11200,7 +11200,7 @@ TEST_P(ExprTest, Utf8Test) {
 
 } // namespace impala
 
-INSTANTIATE_TEST_CASE_P(Instantiations, ExprTest, ::testing::Values(
+INSTANTIATE_TEST_SUITE_P(Instantiations, ExprTest, ::testing::Values(
   //              disable_codegen  enable_expr_rewrites
   std::make_tuple(true,            false),
   std::make_tuple(false,           false),
